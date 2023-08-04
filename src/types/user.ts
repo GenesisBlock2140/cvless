ce1a3e68770d4e72747b13a64419395f5cdac966
GenesisBlock2140 <combnathan02@gmail.com>
2023-08-04 21:27:32 +0200

    Adding mongoose setup files + connection

---
+++ src/types/user.ts
@@ -0,0 +1,11 @@
+export interface IUser {
+  username: string;
+  email: string;
+  password: string;
+  isVerified: boolean;
+  isAdmin: boolean;
+  forgotPasswordToken: string;
+  forgotPasswordTokenExpiry: Date;
+  verifyToken: string;
+  verifyTokenExpiry: Date;
+}
